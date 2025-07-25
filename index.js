f5c14800c503ea827be75ab3dee434f7898ec466
aaminanadem <aaminanadem@gmail.com>
2025-07-25 19:38:46 +0500

    refactoring

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+import{initializeApp} from"https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 const appSettings={
     databaseURL:"https://playground-e7828-default-rtdb.asia-southeast1.firebasedatabase.app/"
-    
+
 }
 
-
+const app= initializeApp
 
 const inputFieldEL = document.getElementById("input-field")
 const addButtonEL = document.getElementById("add-button")
